52674acf1beddd24fb2146a1567d534b1feff20e
Sumitra Mahato <55947732+Ghostz-maker@users.noreply.github.com>
2021-11-26 16:57:03 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,7 @@
+#present year - born year = Age
+
+
+presentyear=input("Present Year: ")
+bornyear=input("Born Year: ") #bornyear is accepted born year is not accepted in varaiables name
+Age= int(presentyear)-int(bornyear)
+print("You are",Age ,"years old")
